bd3319131e3ae72c68fe94f4e9668d545f174b52
Kevin Gomez <kevin.gomez6@upr.edu>
2025-05-06 20:38:16 -0400

    Measurements from CAD

--- include/aon/constants.hpp
+++ include/aon/constants.hpp
@@ -14,30 +14,30 @@
 
 // NOT using 15 inch robot = Using 18 inch robot
 #define USING_15_INCH_ROBOT true
-#define TESTING_AUTONOMOUS true
+#define TESTING_AUTONOMOUS false 
 #define COLOR RED
 
 #if USING_15_INCH_ROBOT
 
-#define SENSITIVITY_DECREASE 800 // 20 works good, currently undergoing testing
+#define SENSITIVITY 3 // 3-10 works good, currently undergoing testing // Higher is more sensitivity
 #define DRIVE_WHEEL_DIAMETER 3.25
 #define TRACKING_WHEEL_DIAMETER 1.959
-#define DISTANCE_LEFT_TRACKING_WHEEL_CENTER 5.650
-#define DISTANCE_RIGHT_TRACKING_WHEEL_CENTER 5.650
-#define DISTANCE_BACK_TRACKING_WHEEL_CENTER 5.500
-#define MOTOR_TO_DRIVE_RATIO .8
+#define DISTANCE_LEFT_TRACKING_WHEEL_CENTER 1.572
+#define DISTANCE_RIGHT_TRACKING_WHEEL_CENTER 1.572
+#define DISTANCE_BACK_TRACKING_WHEEL_CENTER 1.572
+#define MOTOR_TO_DRIVE_RATIO .8 // NumTeethMotorGear / NumTeethWheelGear
 #define GYRO_ENABLED true
 #define GYRO_CONFIDENCE 1
 #define GYRO_FILTER_LENGTH 1
-#define ENCODER_CONFIDENCE 1 - GYRO_CONFIDENCE
+#define ENCODER_CONFIDENCE 0
 #define OFFSET_X_ENCODER_MID 3.250
 
-#define DRIVE_WIDTH 12.478 //distance between front wheels
-#define DRIVE_LENGTH 10 //distance from back wheel to front wheel
-#define DISTANCE_FRONT_LEFT_DRIVE_WHEEL_CENTER 7.99531869284
-#define DISTANCE_BACK_LEFT_DRIVE_WHEEL_CENTER 7.99531869284
-#define DISTANCE_FRONT_RIGHT_DRIVE_WHEEL_CENTER 7.99531869284 //PYTHAG
-#define DISTANCE_BACK_RIGHT_DRIVE_WHEEL_CENTER 7.99531869284
+#define DRIVE_WIDTH 12.15 //distance between front wheels
+#define DRIVE_LENGTH 9 //distance from back wheel to front wheel
+#define DISTANCE_FRONT_LEFT_DRIVE_WHEEL_CENTER 7.55
+#define DISTANCE_BACK_LEFT_DRIVE_WHEEL_CENTER 7.55
+#define DISTANCE_FRONT_RIGHT_DRIVE_WHEEL_CENTER 7.55 //PYTHAG
+#define DISTANCE_BACK_RIGHT_DRIVE_WHEEL_CENTER 7.55
 #define AVG_DRIVETRAIN_RADIUS (DISTANCE_FRONT_LEFT_DRIVE_WHEEL_CENTER + DISTANCE_BACK_LEFT_DRIVE_WHEEL_CENTER + DISTANCE_FRONT_RIGHT_DRIVE_WHEEL_CENTER + DISTANCE_BACK_RIGHT_DRIVE_WHEEL_CENTER) / 4
 // This number may be dependent on the degrees being turn in which case it will not be a constant
 #define CLOCKWISE_ROTATION_DEGREES_OFFSET 0
@@ -48,20 +48,20 @@
 #define INITIAL_ODOMETRY_THETA 0.0
 
 // These next four (4) are in meters (all else is inches)
-#define GPS_X_OFFSET 0.139699924562
-#define GPS_Y_OFFSET 0.215899883414
-#define GPS_INITIAL_X -0.34
-#define GPS_INITIAL_Y -0.82
-#define GPS_INITIAL_HEADING 296.86 // Degrees
+#define GPS_X_OFFSET 0 // CAD
+#define GPS_Y_OFFSET 0.154304916675 // CAD
+#define GPS_INITIAL_X -0.34 // Field
+#define GPS_INITIAL_Y -0.82 // Field
+#define GPS_INITIAL_HEADING 296.86 // Field (in Degrees)
 
-#define MAX_SPEED DRIVE_WHEEL_DIAMETER * M_PI * (int)driveFull.getGearing() / 60
+#define MAX_RPM (int)driveFull.getGearing()
 #define INTAKE_VELOCITY (int)intake.getGearing()
 
 
 #else
 
 
-#define SENSITIVITY_DECREASE 30 // 20 works good, currently undergoing testing
+#define SENSITIVITY 30 // 20 works good, currently undergoing testing
 #define DRIVE_WHEEL_DIAMETER 3.25
 #define TRACKING_WHEEL_DIAMETER 1.959
 #define DISTANCE_LEFT_TRACKING_WHEEL_CENTER 5.650
